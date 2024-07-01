0d2c448decbe35dd75c52f963f43a95383adcec0
Yifei Gao <leng_ning@msn.com>
2024-07-01 19:20:28 +0800

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
-import { API_BASE_URL } from '@/api'
+import { API_BASE_URL } from './src/api.js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -27,6 +27,7 @@ export default defineConfig({
     }
   },
   server: {
+    port: 9996,
     proxy: {
       '/api': {
         target: API_BASE_URL,
